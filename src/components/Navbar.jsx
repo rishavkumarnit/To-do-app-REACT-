e01b6f99150f4ec318918e251b0f40d435ec47fd
Rishav <rishavkumar.nit@gmail.com>
2025-11-08 01:27:46 +0530

    updated navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,18 @@ const Navbar = () => {
   return (
     <nav className="flex fixed justify-between w-full bg-slate-700 py-1 text-white gap-8 h-10">
       <div className="logo">
-        <span className="font-semibold text-lg mx-10">TO DO APP</span>
+        {/* <span className="sm:font-semibold sm:text-lg w-[1/3] ml-5 sm:box-border"> */}
+        <span className="hidden sm:inline sm:font-semibold sm:text-lg w-[1/3] ml-5 sm:box-border">
+          TO DO APP
+        </span>
       </div>
       <ul className="flex gap-14 mx-10">
-        <li  className="hover:text-black hover:cursor-pointer"><a href="#"></a> Home</li>
-        <li className="hover:text-black hover:cursor-pointer"><a href="#todos"></a> My tasks</li>
+        <li className="hover:text-black hover:cursor-pointer">
+          <a href="#"></a> Home
+        </li>
+        <li className="hover:text-black w-16 hover:cursor-pointer">
+          <a href="#todos"></a> My tasks
+        </li>
       </ul>
     </nav>
   );
